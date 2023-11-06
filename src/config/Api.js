a1f420eb851be246a17659bb545f7ac4d696622b
webdeveloper510 <developerweb6@gmail.com>
2023-11-06 12:32:59 +0530

    Updated Live CHanges 06-11-23

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
 export const API = {
-  // BASE_URL: "http://16.170.254.147:8000/",
-  BASE_URL: "http://127.0.0.1:8000/",
+  BASE_URL: "http://16.170.254.147:8000/",
+  // BASE_URL: "http://127.0.0.1:8000/",
 };
